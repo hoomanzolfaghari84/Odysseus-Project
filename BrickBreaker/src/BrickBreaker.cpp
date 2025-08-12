928dbe1a86a21b7bfcdd765b9f09d08fcb5804ac
hoomanzolfaghari <hoomanzolfaghari84@gmail.com>
2025-08-12 19:25:33 +0330

    Add project files.

---
+++ BrickBreaker/src/BrickBreaker.cpp
@@ -0,0 +1,9 @@
+#include <Odysseus2D.hpp>
+
+int main() {
+
+
+	std::cout << "default dir: " << std::filesystem::current_path() << std::endl;
+
+	std::cout << "hello\n";
+}
---
+++ Odysseus2D/src/Odysseus2D.cpp
@@ -0,0 +1,12 @@
+﻿// Odysseus2D.cpp : Defines the entry point for the application.
+//
+
+#include "Odysseus2D.h"
+
+using namespace std;
+
+void engine_test()
+{
+	cout << "Hello CMake." << endl;
+	
+}
